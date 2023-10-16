69dd7232d02795139890e6eea2a1e8ff6edab9e1
nikita <nikita.derjugin@gmail.com>
2023-10-17 00:54:56 +0300

    using HashRouter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,13 @@
-import React, { useEffect } from "react"
-import { useSelector } from "react-redux"
-import { BrowserRouter } from "react-router-dom"
-import { CircularProgress } from "@mui/material"
-import { ErrorSnackbar } from "common/components"
-import { useActions } from "common/hooks"
-import { selectIsInitialized } from "app/appSelectors"
-import { authThunks } from "features/auth/model/authSlice"
-import { Header } from "app/Header/Header"
-import { Routing } from "app/Routing/Routing"
+import React, {useEffect} from "react"
+import {useSelector} from "react-redux"
+import {HashRouter} from "react-router-dom"
+import {CircularProgress} from "@mui/material"
+import {ErrorSnackbar} from "common/components"
+import {useActions} from "common/hooks"
+import {selectIsInitialized} from "app/appSelectors"
+import {authThunks} from "features/auth/model/authSlice"
+import {Header} from "app/Header/Header"
+import {Routing} from "app/Routing/Routing"
 
 function App() {
     const isInitialized = useSelector(selectIsInitialized)
@@ -27,13 +27,13 @@ function App() {
     }
 
     return (
-        <BrowserRouter>
+        <HashRouter>
             <div className="App">
                 <ErrorSnackbar />
                 <Header />
                 <Routing />
             </div>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
